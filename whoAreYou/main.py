e1bd7cb56f809dffb028e458771b88adbbfc6ab2
REli255 <eli.robison@ucas-edu.net>
2024-09-10 10:43:37 -0600

    name, age and color

---
+++ whoAreYou/main.py
@@ -0,0 +1,7 @@
+# Eli Robison, whoAreYou
+
+Name = input("what is your name? :" )
+age = input("what is your age? :" )
+color = input("what is your favorite color? :" )
+
+print( Name,"'s age is", age, "and their favorite color is", color,".")
