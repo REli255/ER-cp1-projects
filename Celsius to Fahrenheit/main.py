2c39a3a12823592c27e0b576939d5f3eea226cbc
REli255 <eli.robison@ucas-edu.net>
2024-09-10 10:35:38 -0600

    transfered code

---
+++ Celsius to Fahrenheit/main.py
@@ -0,0 +1,7 @@
+# Eli Robison, Celsius to Fahrenheit
+
+Celsius= int(input("Enter temperature in Celsius: "))
+
+Farenheit = (Celsius * 9/5)+32
+
+print("When it is", Celsius, "degrees Celsius it is", Farenheit, "degrees Fahrenheit")
