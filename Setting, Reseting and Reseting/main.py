67fcdce02addc115689e231d153537a61bfa0c7a
REli255 <eli.robison@ucas-edu.net>
2024-09-11 10:16:37 -0600

    tables

---
+++ Setting, Reseting and Reseting/main.py
@@ -0,0 +1,13 @@
+# Eli Robison, Award cerimony
+
+staff = 32
+staff = 29
+staff = 30
+students = 100
+students = 99
+guests = ((students-15)*2)+(15*1)
+seats = 12
+
+tables = ((staff+students+guests)/seats)
+
+print("the number of tables needed for the school awards ceremony is",tables)
