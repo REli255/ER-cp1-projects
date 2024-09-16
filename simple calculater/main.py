2b5d23d5816dc4745433d6c892e86048fff2ddf3
REli255 <eli.robison@ucas-edu.net>
2024-09-16 10:52:10 -0600

    +

---
+++ simple calculater/main.py
@@ -0,0 +1,10 @@
+# Eli Robison, simple calculater
+
+type = input("enter the type equation(+, -, *, / or **): ")
+
+first = int(input("enter the first number: "))
+second = int(input("enter the second number: "))
+
+equation = first+second
+
+print(equation)
