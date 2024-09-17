2bff798aa2ee73c66f32d9fef7a761b9a8e19307
REli255 <eli.robison@ucas-edu.net>
2024-09-17 10:31:16 -0600

    calculated

--- simple calculater/main.py
+++ simple calculater/main.py
@@ -1,10 +1,22 @@
 # Eli Robison, simple calculater
 
-type = input("enter the type equation(+, -, *, / or **): ")
+symbol = int(input("enter the type equation(1=+, 2=-, 3=*, 4=/ or 5=**): "))
 
 first = int(input("enter the first number: "))
 second = int(input("enter the second number: "))
 
-equation = first+second
+if symbol == 1:
+    equation = first+second
+else: 
+    if symbol == 2:
+        equation = first-second
+    else: 
+        if symbol == 3:
+            equation = first*second
+        else:
+            if symbol == 4:
+                equation = round(first/second)
+            else:
+                equation = first**second
 
 print(equation)
