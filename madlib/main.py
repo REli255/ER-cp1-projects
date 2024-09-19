5449a918d32868bc3693fed212e50364d4d32c38
REli255 <eli.robison@ucas-edu.net>
2024-09-19 10:51:00 -0600

    attack

---
+++ madlib/main.py
@@ -0,0 +1,70 @@
+#Eli Robison, Madlib
+
+place = input("enter a place: ")
+name = input("enter a name: ")
+object = input("enter a object: ")
+weapon = input("enter a weapon: ")
+food = input("enter a food: ")
+job = input("enter plural of the name for someone who does a specific job (example = police officer): ")
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+print("One day", name, "was visiting", place, "for a business trip. While he was there some", job, "attacked him. He then jumped behind a", object, "to hide.")
