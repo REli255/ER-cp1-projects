fb900fd0ee793b26dcb2d204a568d0e307d91f73
REli255 <eli.robison@ucas-edu.net>
2024-09-20 10:28:36 -0600

    fixed

--- palandrom/main.py
+++ palandrom/main.py
@@ -1,7 +1,7 @@
 # Eli Robison, Palandrom
 
-w = input("enter a seven letter word: ")
-r = str(w[-1] + w[-2] + w[-3] + w[-4] + w[-5] + w[-6] + w[-7])
+w = input("enter a word: ")
+r = w[::-1]
 
 if w == r:
     print(w, "is a palandrom")
