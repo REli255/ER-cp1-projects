a123612edad8b186236cafde16928d0920012c67
REli255 <eli.robison@ucas-edu.net>
2024-09-20 10:50:34 -0600

    math

---
+++ Graded Quiz/main.py
@@ -0,0 +1,7 @@
+#Eli Robison, graded quiz
+
+question_a = input("what does equal: ")
+question_b = input("what does equal: ")
+question_c = input("what does equal: ")
+question_d = input("what does equal: ")
+question_e = input("what does equal: ")
