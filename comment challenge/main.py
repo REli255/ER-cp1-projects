24b277901b277d13777f8cfd515790635c0718bf
REli255 <eli.robison@ucas-edu.net>
2024-10-01 10:31:44 -0600

    named

---
+++ comment challenge/main.py
@@ -0,0 +1,21 @@
+#------------------------------------------------------------------------------------------------------------------
+
+#Name:          comment challenge
+
+#Purpus:        greets the user
+
+#
+
+#Author:        Eli Robison
+
+#
+
+#Created:       10/1/2024
+
+#Licence:        none
+
+#------------------------------------------------------------------------------------------------------------------
+
+name = input("enter name: ")# asks user for their name
+
+print("hello", name)#displays "hello", their name
