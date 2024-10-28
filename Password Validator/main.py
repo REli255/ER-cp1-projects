4ac35b5f56618f083d138c8b67f8fd20627fb0d2
REli255 <eli.robison@ucas-edu.net>
2024-10-28 10:45:20 -0600

    death$4%

--- Password Validator/main.py
+++ Password Validator/main.py
@@ -2,10 +2,34 @@
 
 check = 0
 
-while check == 0:
-    password = input("enter a pasaword: ")
+def contains_number(value):
+    for character in value:
+        if character.isdigit():
+            return True
+    return False
+
+import re
+def contains_special(string):
+    rex = re.compile("@!#$%^&*<>?~")
+    search = rex.search(string)
+    if search == None:
+        return False
+    else:
+        return True
+
+while check < 3:
+    password = input("enter a password: ")
 
     if len(password) <= 8:
-        check = check + 1
-    elif any(password):
-        check = check + 1
+        check += 1
+
+    if contains_number(password):
+        check += 1
+
+    if contains_special(password):
+        check += 1
+
+    if check < 3:
+        print(password, "does not meet the requirements")   
+
+print(password, "meets the requirements")
