8ee768fa6d116c5e452c5a4a9bc0cc5473257eba
REli255 <eli.robison@ucas-edu.net>
2024-11-06 10:51:43 -0700

    CMON

---
+++ Simple Quiz Game/main.py
@@ -0,0 +1,5 @@
+# Eli Robison, ProficiencyTest: Simple Quiz Game
+
+easy_questions = ["Is Zombicide a CMON game?", "Is Is Monopoly a CMON game?", "Is Cthulhu: Death May Die a CMON game?"]
+normal_questions = ["Is Blood Rage a CMON game?", "Is Code Names a CMON game?", "Is Seven Wonders a CMON game?"]
+hard_questions = ["how many miniatures are in the  core box?", "how many miniatures are in the  core box??", "how many miniatures are in the  core box?"]
