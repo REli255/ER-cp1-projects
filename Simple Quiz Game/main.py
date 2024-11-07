81c1677a16646877f13459582228589f168af017
REli255 <eli.robison@ucas-edu.net>
2024-11-07 10:51:27 -0700

    questions[x][y]

--- Simple Quiz Game/main.py
+++ Simple Quiz Game/main.py
@@ -2,4 +2,24 @@
 
 easy_questions = ["Is Zombicide a CMON game?", "Is Is Monopoly a CMON game?", "Is Cthulhu: Death May Die a CMON game?"]
 normal_questions = ["Is Blood Rage a CMON game?", "Is Code Names a CMON game?", "Is Seven Wonders a CMON game?"]
-hard_questions = ["how many miniatures are in the  core box?", "how many miniatures are in the  core box??", "how many miniatures are in the  core box?"]
+hard_questions = ["How many miniatures are in the Zombicide: Undead or Alive core box?", "How many miniatures are in the Cthulhu: Death May Die - Fear of the Unknown core box?", "How many miniatures are in the Massive Darkness 2: Hellscape core box?"]
+questions = [easy_questions, normal_questions, hard_questions]
+
+easy_answers = ["yes", "no", "yes"]
+normal_answers = ["yes", "no", "no"]
+hard_answers = ["88", "46", "68"]
+answers = [easy_answers, normal_answers, hard_answers]
+
+score = 0
+
+for x in range(len(questions)):
+    for y in range(len(questions[x])):
+        guess = input(questions[x][y])
+        if guess == answers[x][y]:
+            score += 1
+            print("you were right")
+        else:
+            print("you were wrong")
+        total = len(questions) * len(questions[x])
+
+print("score is", score, "out of", total)
