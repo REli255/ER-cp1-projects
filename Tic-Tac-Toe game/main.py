f5a61f77785c92f4d449dcc81374189a83277252
REli255 <eli.robison@ucas-edu.net>
2024-11-11 10:39:20 -0700

    cross = "X"

--- Tic-Tac-Toe game/main.py
+++ Tic-Tac-Toe game/main.py
@@ -1,1 +1,14 @@
 # Eli Robison, Tic-Tac-Toe game
+
+def check(letter):
+    for x in range(3):
+        for y in range(3):
+            x, y, letter
+
+cross = "X"
+circle = "O"
+
+if check(cross):
+    print("you won")
+elif check(circle):
+    print("you lost")
